faec04a03ad4bb8645f771fa1978ff3e8dec3692
justindenaro <justindenaro98@gmail.com>
2026-04-19 15:30:49 -0700

    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+import time
+from playwright.sync_api import sync_playwright
+from playwright_stealth import Stealth
+from bs4 import BeautifulSoup
+
+def run_scraper():
+    
+    
+if __name__ == "__main__":
+    run_scraper()
