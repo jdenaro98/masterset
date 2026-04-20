e815f28492895cfe8013b5d000db2b083aca9ff7
Justin <jape1998@gmail.com>
2026-04-19 20:37:18 -0700

    saving

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-import sys
-import time
 from playwright.sync_api import sync_playwright
 from playwright_stealth import Stealth
-from bs4 import BeautifulSoup
 
-def run_scraper():
-    
-    
+def main():
+    # Step 1: Scrape Games List
+    with Stealth().sync_playwright() as pw:
+        URL = "https://www.tcgplayer.com/categories"
+
+        browser = pw.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        page.goto(URL)
+        page.wait_for_load_state("networkidle")
+        page.screenshot(path="debug.png")
+
+        # 1. Target the first grouping container specifically
+        # This specifically targets "Trading/Collectible Card Games"
+        # first_group = page.locator(".grouping").first
+        first_group = page.locator(".site-map.grouping > .grouping").first
+
+        # 2. Find all titles/links within the desired group
+        for link in first_group.get_by_test_id("base-link").all():
+            data = {
+                "title": link.inner_text(),
+                "url": link.get_attribute("href")
+            }
+            # Debug
+            print(data)
+
+        browser.close()
+
 if __name__ == "__main__":
-    run_scraper()
+    main()
